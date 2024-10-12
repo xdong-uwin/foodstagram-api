05bc6dc924d283714bd5f7e4788bc18dd977e7ba
Xiangrui Dong <xiangrui@Xiangruis-MacBook-Pro-2.local>
2024-10-12 11:54:22 -0400

    Initial commit

---
+++ src/main/java/org/scraper/foodstagram/FoodstagramApplication.java
@@ -0,0 +1,13 @@
+package org.scraper.foodstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodstagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodstagramApplication.class, args);
+    }
+
+}
