959ee506c91deb7949111e6856bd7b32a9ed0811
Xiangrui Dong <dong88@uwindsor.ca>
2024-10-21 13:44:38 -0400

    add repository method for querying by email

--- src/main/java/org/scraper/foodstagram/repository/MemberRepository.java
+++ src/main/java/org/scraper/foodstagram/repository/MemberRepository.java
@@ -4,6 +4,8 @@ import org.scraper.foodstagram.repository.entity.Member;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface MemberRepository extends PagingAndSortingRepository<Member, String>, CrudRepository<Member,String> {
+import java.util.Optional;
 
-    }
+public interface MemberRepository extends PagingAndSortingRepository<Member, String>, CrudRepository<Member, String> {
+    Optional<Member> findByEmail(String email);
+}
