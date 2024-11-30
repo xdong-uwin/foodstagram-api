77109e5b82faea0995645aefcc3895bf578dfca0
Xiangrui Dong <dong88@uwindsor.ca>
2024-11-29 22:25:50 -0500

    add PATCH method to allowed methods list

--- src/main/java/org/scraper/foodstagram/config/WebConfig.java
+++ src/main/java/org/scraper/foodstagram/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
             }
         };
     }
