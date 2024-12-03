5e5aa58e616c6b4b09072dae272cf0fc899a154a
Xiangrui Dong <dong88@uwindsor.ca>
2024-12-02 22:49:19 -0500

    modify CORS policy

--- src/main/java/org/scraper/foodstagram/config/WebConfig.java
+++ src/main/java/org/scraper/foodstagram/config/WebConfig.java
@@ -3,10 +3,12 @@ package org.scraper.foodstagram.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@CrossOrigin(origins = "http://64.237.56.213:3000")
 public class WebConfig {
 
     @Bean
@@ -15,8 +17,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://64.237.56.213:3000")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://64.237.56.213:3000", "http://localhost:3000")
+                        .allowedOrigins()
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
             }
         };
